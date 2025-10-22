87cb93f182853770b671b6cdc2fe691b3201b6b6
Allen Shaw <allen@joineryhq.com>
2025-10-22 16:41:44 -0500

    Update mailtolink.php

--- mailtolink.php
+++ mailtolink.php
@@ -23,7 +23,7 @@ function mailtolink_civicrm_buildForm($formName, &$form) {
       'emailHeaderLabels' => array(),
     );
 
-    if ($ufGroupId = $form->_formValues['uf_group_id'] ?? $form->_submitValues['uf_group_id'] ?? NULL) {
+    if ($ufGroupId = ($form->_formValues['uf_group_id'] ?? $form->_submitValues['uf_group_id'] ?? NULL)) {
       $js_vars['isProfileResults'] = TRUE;
       $api_params = array(
         'uf_group_id' => $ufGroupId,
